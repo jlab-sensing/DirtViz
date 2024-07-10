e81d711e60636cc4ed5cc39b8059156a13686ee1
Aaron Wu <12989649+Aaron-Wu1@users.noreply.github.com>
2024-07-10 10:13:19 -0700

    added queue name

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -76,7 +76,7 @@ def create_app(debug: bool = False) -> Flask:
                     "region": os.getenv("AWS_DEFAULT_REGION"),
                     "visibility_timeout": timedelta(minutes=15).total_seconds(),
                     "predefined_queues": {
-                        "celery": {  ## the name of the SQS queue
+                        "dirtviz-task-queue": {  ## the name of the SQS queue
                             "url": os.getenv("AWS_SQS_URL"),
                             "access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
                             "secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
@@ -97,15 +97,7 @@ def create_app(debug: bool = False) -> Flask:
                     result_backend=os.getenv("CELERY_RESULT_BACKEND"),
                     task_ignore_result=True,
                     broker_transport_options={
-                        "region": os.getenv("AWS_DEFAULT_REGION"),
                         "visibility_timeout": timedelta(minutes=15).total_seconds(),
-                        "predefined_queues": {
-                            "celery": {  ## the name of the SQS queue
-                                "url": os.getenv("AWS_SQS_URL"),
-                                "access_key_id": os.getenv("AWS_ACCESS_KEY_ID"),
-                                "secret_access_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-                            }
-                        },
                     },
                     task_create_missing_queues=False,
                     task_ack_late=True,
